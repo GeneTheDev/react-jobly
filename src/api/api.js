34d5176bb7393d47cac66e16adb23f80db87baaa
gene21gene <owens.eugene@yahoo.com>
2023-04-29 19:07:50 -0400

    updating import statments and api route

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.React_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class JoblyApi {
   // the token for interactive with the APi will be stored here.
